db01de0da55233ffddd7ba89976b0753fb360adb
Development Team <dev@company.com>
2026-03-29 16:34:45 +0530

    Add author rewrite script

---
+++ rewrite_author.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export GIT_AUTHOR_NAME="Development Team"
+export GIT_AUTHOR_EMAIL="dev@company.com"
+export GIT_COMMITTER_NAME="Development Team"
+export GIT_COMMITTER_EMAIL="dev@company.com"
+
